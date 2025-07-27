21eb8e6ef163eb5186b6c5a5b5aaf6e355e1d443
Subav11 <subhamyadav7528@gmail.com>
2025-07-27 18:11:39 +0530

    update

--- server.js
+++ server.js
@@ -11,24 +11,24 @@ app.use(cors());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose
-  .connect(`mongodb://localhost:27017/todo`)
-  .then(() => {
-    app.listen(8081, () => {
-      console.log("Server Started");
-    });
-  })
-  .catch((err) => console.log("DB Error", err));
-
 // mongoose
-//   .connect(
-//     `mongodb+srv://${dbuser}:${dbpass}@cluster0.ts55vzb.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
-//   )
+//   .connect(`mongodb://localhost:27017/todo`)
 //   .then(() => {
 //     app.listen(8081, () => {
-//       console.log("Server started");
+//       console.log("Server Started");
 //     });
-//   }).catch((err) => console.log("DB Error", err));
+//   })
+//   .catch((err) => console.log("DB Error", err));
+
+mongoose
+  .connect(
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.ts55vzb.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(8081, () => {
+      console.log("Server started");
+    });
+  }).catch((err) => console.log("DB Error", err));
 
 app.use("/api/users", userRouter);
 app.use("/api/todo", todoRouter);
